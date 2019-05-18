741b6b5c9923203a1b5c136e2f23a8dd29268e66
Andrew Short <andrewjshort@gmail.com>
2019-05-18 17:15:58 +1000

    Align logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/logo.svg';
 
 const Header = () => (
-  <Navbar bg='dark' expand='md' variant='dark'>
+  <Navbar bg='dark' className='Header' expand='md' variant='dark'>
     <Container>
       <Navbar.Brand>
         <img src={logo} alt='SES Logo' width={20} height={20} /> WOL SES
