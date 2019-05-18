8e9f004392557be831272550d2ed28cfbd9f9766
Andrew Short <andrewjshort@gmail.com>
2019-05-18 16:02:25 +1000

    Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import logo from '../assets/logo.svg';
+
 const Header = () => (
   <Navbar bg='dark' expand='md' variant='dark'>
     <Container>
-      <Navbar.Brand>WOL SES</Navbar.Brand>
+      <Navbar.Brand>
+        <img src={logo} alt='SES Logo' width={20} height={20} /> WOL SES
+      </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav>
