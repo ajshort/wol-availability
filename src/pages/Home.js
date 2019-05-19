bfd5d799adf2387fe6b2c56e31af8e72c941bead
Andrew Short <andrewjshort@gmail.com>
2019-05-19 23:24:00 +1000

    Available members card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
+import gql from 'graphql-tag';
+import moment from 'moment';
 import React, { useEffect } from 'react';
+import { Query } from 'react-apollo';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Spinner from 'react-bootstrap/Spinner';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import { getDocumentTitle } from '../utils';
+
+const AVAILABLE_MEMBERS_QUERY = gql`
+  query ($instant: DateTime!) {
+    membersAvailable(instant: $instant) {
+      fullName
+      surname
+    }
+  }
+`;
 
 const Home = () => {
   useEffect(() => {
@@ -22,6 +36,39 @@ const Home = () => {
             </Button>
           </LinkContainer>
         </Card.Header>
+        <Query query={AVAILABLE_MEMBERS_QUERY} variables={{ instant: moment().format() }}>
+          {({ loading, error, data }) => {
+            if (loading) {
+              return (
+                <Card.Body>
+                  <Spinner animation='border' size='sm' /> Loading available members&hellip;
+                </Card.Body>
+              );
+            }
+
+            if (error) {
+              return (
+                <Card.Body className='text-danger'>Error loading available members.</Card.Body>
+              );
+            }
+
+            const members = data.membersAvailable.sort((a, b) => (
+              a.surname.localeCompare(b.surname)
+            ));
+
+            if (!members || members.length === 0) {
+              return <Card.Body>There are no members available.</Card.Body>;
+            }
+
+            return (
+              <ListGroup variant='flush'>
+                {members.map((member) => (
+                  <ListGroup.Item>{member.fullName}</ListGroup.Item>
+                ))}
+              </ListGroup>
+            )
+          }}
+        </Query>
       </Card>
     </Container>
   )
