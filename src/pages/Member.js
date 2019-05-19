7e1f59345d38c547d8290d2f380c6dbfcad41c52
Andrew Short <andrewjshort@gmail.com>
2019-05-20 00:51:34 +1000

    Redirect me page

--- src/pages/Member.js
+++ src/pages/Member.js
@@ -1,13 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
 
-import { getDocumentTitle } from '../utils';
+import AuthContext from '../components/AuthContext';
+import { getDocumentTitle, getWeekStart } from '../utils';
 
-const Member = () => {
+const MePage = () => {
+  const { member } = useContext(AuthContext);
+  const week = getWeekStart().format('YYYY-MM-DD');
+
+  return <Redirect to={`/member/${member.number}/${week}`} />;
+};
+
+const Member = ({ match }) => {
   useEffect(() => {
     document.title = getDocumentTitle('Member Availability');
   });
 
-  return '';
+  return (
+    <Switch>
+      <Route path={`${match.path}/:member/:week`} />
+      <Route path={`${match.path}/me`} component={MePage} />
+      <Route path={`${match.path}/`} />
+    </Switch>
+  )
 }
 
-export default Member;
+export default withRouter(Member);
