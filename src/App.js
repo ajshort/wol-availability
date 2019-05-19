bbbec4d6a4ace695cd4e819dc710ff3813c3ed56
Andrew Short <andrewjshort@gmail.com>
2019-05-19 21:42:18 +1000

    Add bearer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -9,8 +12,23 @@ import Login from './pages/Login';
 import Member from './pages/Member';
 import Unit from './pages/Unit';
 
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_API_URI,
+});
 
-const client = new ApolloClient({ uri: process.env.REACT_APP_API_URI })
+const authLink = setContext((_operation, { headers }) => {
+  const token = localStorage.getItem('token');
+  const authorization = token ? `Bearer ${token}` : '';
+
+  return {
+    headers: { authorization, ...headers }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 const App = () => (
   <ApolloProvider client={client}>
