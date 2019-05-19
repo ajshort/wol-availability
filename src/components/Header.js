13663c7e470bf1112da93526569dcdedeea77cb0
Andrew Short <andrewjshort@gmail.com>
2019-05-19 23:01:19 +1000

    Hide nav links when not logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import { FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import logo from '../assets/logo.svg';
+import { AuthConsumer } from '../components/AuthContext';
 
 const NavLink = ({ children, to, ...props }) => (
   <LinkContainer to={to} {...props}>
@@ -14,32 +15,44 @@ const NavLink = ({ children, to, ...props }) => (
   </LinkContainer>
 );
 
+const Brand = (props) => (
+  <Navbar.Brand {...props}>
+    <img src={logo} alt='SES Logo' width={20} height={20} /> WOL SES
+  </Navbar.Brand>
+);
+
 const Header = () => (
   <Navbar bg='dark' className='Header' expand='md' variant='dark'>
     <Container>
-      <LinkContainer to='/' exact>
-        <Navbar.Brand>
-          <img src={logo} alt='SES Logo' width={20} height={20} /> WOL SES
-        </Navbar.Brand>
-      </LinkContainer>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Nav>
-          <NavLink to='/' exact>Home</NavLink>
-          <NavLink to='/member'>Member</NavLink>
-          <NavLink to='/unit'>Unit</NavLink>
-        </Nav>
-        <Nav className='ml-auto'>
-          <NavDropdown title={<><FaUser /> User Name</>}>
-            <LinkContainer to='/member/me'>
-              <NavDropdown.Item>My availability</NavDropdown.Item>
+      <AuthConsumer>
+        {({ member }) => (member ? (
+          <React.Fragment>
+            <LinkContainer to='/' exact>
+              <Brand />
             </LinkContainer>
-            <LinkContainer to='/logout'>
-              <NavDropdown.Item>Logout</NavDropdown.Item>
-            </LinkContainer>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+              <Nav>
+                <NavLink to='/' exact>Home</NavLink>
+                <NavLink to='/member'>Member</NavLink>
+                <NavLink to='/unit'>Unit</NavLink>
+              </Nav>
+              <Nav className='ml-auto'>
+                <NavDropdown title={<><FaUser /> {member.fullName}</>}>
+                  <LinkContainer to='/member/me'>
+                    <NavDropdown.Item>My availability</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/logout'>
+                    <NavDropdown.Item>Logout</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </React.Fragment>
+        ) : (
+          <Brand className='mx-auto' />
+        ))}
+      </AuthConsumer>
     </Container>
   </Navbar>
 );
