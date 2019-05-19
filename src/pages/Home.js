36ba594aa8bf743f6b8647233262c384eae401cb
Andrew Short <andrewjshort@gmail.com>
2019-05-19 23:07:22 +1000

    Card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,8 @@
 import React, { useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 import { getDocumentTitle } from '../utils';
 
@@ -7,7 +11,20 @@ const Home = () => {
     document.title = getDocumentTitle('Home');
   });
 
-  return '';
+  return (
+    <Container className='my-3'>
+      <Card>
+        <Card.Header className='d-flex justify-content-between align-items-center'>
+          Available Members
+          <LinkContainer to='/member/me'>
+            <Button variant='primary' size='sm'>
+              My availability
+            </Button>
+          </LinkContainer>
+        </Card.Header>
+      </Card>
+    </Container>
+  )
 }
 
 export default Home;
