a1c8d2cc1dd02c16351cb6ea8ab0b147a69765e1
Andrew Short <andrewjshort@gmail.com>
2019-06-25 10:57:52 +1000

    If already logged in redirect to home

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
+import { Redirect } from 'react-router';
 
+import { AuthConsumer } from '../components/AuthContext';
 import LoginForm from '../components/LoginForm';
 import { getDocumentTitle } from '../utils';
 
@@ -10,9 +13,27 @@ const Login = () => {
   });
 
   return (
-    <Container className='Login'>
-      <LoginForm />
-    </Container>
+    <AuthConsumer>
+      {({ loading, member }) => {
+        if (loading) {
+          return (
+            <div className='d-flex justify-content-center my-3'>
+              <Spinner animation='border' />
+            </div>
+          );
+        }
+
+        if (member) {
+          return <Redirect to='/' />;
+        }
+
+        return (
+          <Container className='Login'>
+            <LoginForm />
+          </Container>
+        );
+      }}
+    </AuthConsumer>
   );
 };
 
