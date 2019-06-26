dd28e4befcc3cc6876a9eca56fa6943c06cdbf29
Andrew Short <andrewjshort@gmail.com>
2019-06-26 14:20:27 +1000

    Log ID

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -1,21 +1,48 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
 import { withRouter } from 'react-router-dom';
 
-const Analytics = ({ children, history, location, ua }) => {
-  const view = (location) => {
-    console.log(location.pathname);
-    ReactGA.pageview(location.pathname);
-  };
+import { AuthConsumer } from '../components/AuthContext';
+
+const AnalyticsContent = ({ children, history, location, loading, member }) => {
+  useEffect(() => {
+    ReactGA.set({ userId: member ? member.number : undefined });
+  }, [member]);
 
   useEffect(() => {
-    ReactGA.initialize(ua);
-    view(location);
-
-    history.listen(view);
-  }, []);
+    if (!loading) {
+      ReactGA.pageview(location.pathname);
+      history.listen(location => ReactGA.pageview(location.pathname));
+    }
+  }, [loading]);
 
   return children;
+};
+
+const Analytics = ({ children, history, location, ua }) => {
+  useEffect(() => {
+    ReactGA.initialize(ua);
+  }, [ua]);
+
+  return (
+    <AuthConsumer>
+      {({ loading, member }) => {
+        if (loading) {
+          return children;
+        }
+
+        return (
+          <AnalyticsContent
+            children={children}
+            history={history}
+            location={location}
+            loading={loading}
+            member={member}
+          />
+        );
+      }}
+    </AuthConsumer>
+  );
 };
 
 export default withRouter(Analytics);
