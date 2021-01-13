f4e0fd31d3bb90644443ee8839938a72fa8fa196
maybz <amayberry87@gmail.com>
2021-01-13 14:09:40 +1100

    Added Ranks (SGO and Inspector)

--- src/components/RankImage.js
+++ src/components/RankImage.js
@@ -4,6 +4,8 @@ import jr from '../assets/job-ready.svg';
 import lso from '../assets/leading-senior-operator.svg';
 import o from '../assets/operator.svg';
 import so from '../assets/senior-operator.svg';
+import sgo from '../assets/senior-group-officer.svg';
+import ins from '../assets/inspector.svg';
 
 export default ({ rank, ...props }) => {
   if (rank === 'Job Ready') {
@@ -20,6 +22,14 @@ export default ({ rank, ...props }) => {
 
   if (rank === 'Leading Senior Operator') {
     return <img src={lso} alt={rank} title={rank} {...props} />;
+  }
+
+  if (rank === 'Senior Group Officer') {
+    return <img src={sgo} alt={rank} title={rank} {...props} />;
+  }
+
+  if (rank === 'Inspector') {
+    return <img src={ins} alt={rank} title={rank} {...props} />;
   }
 
   return null;
