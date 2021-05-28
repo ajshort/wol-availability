afed13533a0d21664b8a891c7c8e0a8f78ef246e
Andrew Short <andrewjshort@gmail.com>
2021-05-28 15:10:29 +1000

    Add some unit config

---
+++ src/config/units.ts
@@ -0,0 +1,11 @@
+interface UnitConfig {
+  stormUnits: string[];
+  rescueUnits: string[];
+}
+
+export const UNIT_CONFIGS: { [code: string]: UnitConfig } = {
+  'SEZ-NIC-WOL': {
+    stormUnits: ['SEZ-NIC-WOL'],
+    rescueUnits: ['SEZ-NIC-WOL', 'SEZ-NIC-DPT'],
+  },
+};
