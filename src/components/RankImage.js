61e5103297256616b5dde1d54177d9888610144f
Andrew Short <andrewjshort@gmail.com>
2021-05-28 15:43:53 +1000

    More ranks

--- src/components/RankImage.js
+++ src/components/RankImage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import ci from '../assets/chief-inspector.svg';
 import jr from '../assets/job-ready.svg';
 import lso from '../assets/leading-senior-operator.svg';
 import o from '../assets/operator.svg';
 import so from '../assets/senior-operator.svg';
+import go from '../assets/group-officer.svg';
 import sgo from '../assets/senior-group-officer.svg';
 import ins from '../assets/inspector.svg';
 
@@ -24,12 +26,20 @@ export default ({ rank, ...props }) => {
     return <img src={lso} alt={rank} title={rank} {...props} />;
   }
 
+  if (rank === 'SES Group Officer') {
+    return <img src={go} alt={rank} title={rank} {...props} />;
+  }
+
   if (rank === 'SES Senior Group Officer') {
     return <img src={sgo} alt={rank} title={rank} {...props} />;
   }
 
   if (rank === 'SES Inspector') {
     return <img src={ins} alt={rank} title={rank} {...props} />;
+  }
+
+  if (rank === 'SES Chief Inspector' || rank === 'SES Local Controller') {
+    return <img src={ci} alt={rank} title={rank} {...props} />;
   }
 
   if (rank) {
